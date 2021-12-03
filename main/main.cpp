e2cb869920dc368ab0017c745f4224de710b4eb3
steven <wdchien@kth.se>
2021-12-03 17:47:41 +0100

    isolated version

---
+++ main/main.cpp
@@ -0,0 +1,63 @@
+#include <fftw3.h>
+#include <cstring>
+#include <gtest/gtest.h>
+#include <immintrin.h>
+#include <iostream>
+#include <omp.h>
+#include <random>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include "dft_c2cf_12.c"
+#include "utils.h"
+
+int main(int argc, char *argv[])
+{
+  //c2c
+  const int fft_size = 12;
+  const int batch_size = 2;
+  __m512* xt = nullptr;
+  __m512* xf = nullptr;
+  constexpr size_t byte_size = 2 * fft_size * batch_size * sizeof(float);
+
+  ::posix_memalign((void**)(&xt), 64, byte_size);
+  ::posix_memalign((void**)(&xf), 64, byte_size);
+
+  std::random_device rd;
+  std::mt19937 generator(rd());
+  std::uniform_real_distribution<float> dist(-10.0, 10.0);
+  //Store values in vector
+  std::vector<float> values(2 * fft_size * batch_size);
+  for (int i = 0; i < values.size(); ++i) {
+    values[i] = dist(generator);
+  }
+  fftwf_complex* xt_fftw = fftwf_alloc_complex(fft_size * batch_size);
+  fftwf_complex* xf_fftw = fftwf_alloc_complex(fft_size * batch_size);
+
+  fftwf_plan plan = fftwf_plan_many_dft(
+      1, &fft_size, batch_size, xt_fftw, &fft_size, 1, fft_size,
+      xf_fftw, &fft_size, 1, fft_size, FFTW_FORWARD, FFTW_MEASURE);
+
+//  fftwf_plan plan = fftwf_plan_many_dft(
+//      1, &fft_size, batch_size, xt_fftw, &fft_size, 1, 0,
+//      xf_fftw, &fft_size, 1, 0, FFTW_FORWARD, FFTW_MEASURE);
+
+  for (int i = 0; i < values.size(); i += 2) {
+    xt_fftw[i / 2][0] = values[i];
+    xt_fftw[i / 2][1] = values[i + 1];
+  }
+
+  fftwf_execute(plan);
+
+  std::vector<float> out_array(2 * fft_size * batch_size);
+  //avx512_gather(fft_size, batch_size, xt, &values[0]);
+  //dft_codelet_c2cf_32(xt, xt + 1, xf, xf + 1, 2, 2, batch_size / 16, (2 * fft_size), (2 * fft_size));
+  dft_c2cf_12(xt, xt + 1, xf, xf + 1, 2, 2, 1, 0, 0);
+
+  //avx512_scatter(fft_size, batch_size, xf, &out_array[0]);
+
+  for (int i = 0; i < 2 * fft_size * batch_size; i += 2) {
+    std::cerr << "ours[" << i / 2 << "]\t Real: " << out_array[i] << ", complex: " << out_array[i + 1] << "\n";
+    std::cerr << "FFTW[" << i / 2 << "]\t Real: " << xf_fftw[i/2][0] << ", complex: " << xf_fftw[i/2][1] << "\n\n";
+  }
+}
---
+++ main/utils.cpp
@@ -0,0 +1,29 @@
+#include "utils.h"
+
+#include <immintrin.h>
+
+constexpr int nvec_512 = 16;
+
+void avx512_gather(int fft_size, int batch_size, __m512* simd_arr, float* in_data) {
+    int num_floats = 2 * fft_size;
+
+  __m512i vIdx = _mm512_set_epi32(
+      num_floats * 15, num_floats * 14, num_floats * 13, num_floats * 12, num_floats * 11, num_floats * 10, num_floats * 9,
+      num_floats * 8, num_floats * 7, num_floats * 6, num_floats * 5, num_floats * 4, num_floats * 3, num_floats * 2, num_floats, 0);
+    for (unsigned i = 0; i < batch_size / nvec_512; i++)
+        for (unsigned j = 0; j < num_floats; j++) {
+            float* ptr = in_data + j + i * num_floats * nvec_512;
+            simd_arr[j + i * num_floats] = _mm512_i32gather_ps(
+                vIdx, static_cast<void*>(ptr), 4);
+    }
+}
+
+void avx512_scatter(int fft_size, int batch_size, __m512* simd_arr, float* out_data) {
+    int num_floats = 2 * fft_size;
+    for (unsigned i = 0; i < batch_size / nvec_512; i++)
+        for (unsigned j = 0; j < num_floats; j++)
+            for (unsigned k = 0; k < nvec_512; k++) {
+                out_data[i * num_floats * nvec_512 + k * num_floats + j] =
+                    simd_arr[j + i * num_floats][k];
+            }
+}
